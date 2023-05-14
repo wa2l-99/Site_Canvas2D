b3939567c9b6d851f2604e98581d24311d83d01d
wa2l-99 <waelzantour1@gmail.com>
2023-05-14 15:05:21 +0200

    chore: create config_file for the connection to bd

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+try {
+	// On se connecte à MySQL en utilsant pdo
+	$bdd = new PDO("mysql:host=localhost;dbname=site_canvas2d;charset=utf8", "root", "root");
+	$bdd->query("set character set utf8");
+} catch (Exception $e) {
+	// En cas d'erreur, on affiche un message et on arrête tout
+	die('Erreur : ' . $e->getMessage());
+}
